0b47476e40f867ae0926b05bd941d03cc6830e7a
mcwhare <marcus.chanwh@gmail.com>
2026-09-21 21:20:08 +1000

    reverted mail edits

--- src/mailer.js
+++ src/mailer.js
@@ -2,15 +2,12 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: 25,
-  secure: false,
+  port: Number(process.env.SMTP_PORT || 465),
+  secure: process.env.SMTP_SECURE !== 'false',
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
   },
-  tls: {
-    rejectUnauthorized: false,
-  }
 });
 
 /**
